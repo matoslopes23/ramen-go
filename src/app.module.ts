a3b40b7151a80db6dc2c22d25c595b078c25d57b
matoslopes23 <matoslopes23@gmail.com>
2024-06-03 20:01:45 -0300

    feat: create new module broths

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiKeyMiddleware } from './common/middlewares/api-key.middleware';
+import { BrothsModule } from './broths/broths.module';
+import { BrothsController } from './broths/broths.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [BrothsModule],
+  controllers: [AppController, BrothsController],
   providers: [AppService],
 })
 export class AppModule {
---
+++ src/modules/broths/controllers/broths.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('broths')
+export class BrothsController {}
