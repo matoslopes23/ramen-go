07b0b4f34ec00924774538f53b03ce7e8a6d6fcf
matoslopes23 <matoslopes23@gmail.com>
2024-06-03 22:42:47 -0300

    feat: integration to api generate order id

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ApiKeyMiddleware } from './common/middlewares/api-key.middleware';
 import { BrothsModule } from './modules/broths/broths.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProteinsModule } from './modules/proteins/proteins.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BrothsModule, ProteinsModule],
+  imports: [ConfigModule.forRoot(), BrothsModule, ProteinsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/orders/services/orders.service.ts
+++ src/modules/orders/services/orders.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class OrdersService {
   private orders = [];
 
-  create(order) {
+  async create(order) {
     const newOrder = {
-      id: this.orders.length + 1,
+      id: await this.generateOrderId(),
       ...order,
     };
     this.orders.push(newOrder);
     return newOrder;
+  }
+
+  private async generateOrderId(): Promise<string> {
+    try {
+      const response = await axios.post(
+        'https://api.tech.redventures.com.br/orders/generate-id',
+        {},
+        {
+          headers: {
+            'x-api-key': 'ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf',
+          },
+        },
+      );
+      return response.data.id;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to generate order ID',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
